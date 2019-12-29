41b51d653e64c6e5260b86f63c28e0dfdfd2ed87
lgope <ilakshmangope@gmail.com>
2019-12-29 23:16:55 +0600

    final touch 🗽🚀

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Laptop-Store
+# The-Laptop-Store
 This is a laptop store demo web. And it's my first nodejs web project. 🙂
+
+Here is the deployed version 👉 https://the-laptop-shop.herokuapp.com/
---
+++ index.js
@@ -0,0 +1,87 @@
+const port = process.env.PORT || 5000;
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8'); // __dirname is basically the home folder. fisrt arg is file location and second is character in coding
+
+const laptopData = JSON.parse(json); // it's will return's an object
+
+
+const server = http.createServer((req, res) => {
+
+    const pathName = url.parse(req.url, true).pathname;
+    const id = url.parse(req.url, true).query.id;
+
+
+    // PRODUCTS OVERVIEW
+    if (pathName === '/products' || pathName === '/') {
+
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+
+        fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) => {
+            let overviewOutput = data;
+
+            fs.readFile(`${__dirname}/templates/template-card.html`, 'utf-8', (err, data) => {
+                const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join(''); // we used join() to see the output as string
+                overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput);
+                res.end(overviewOutput);
+            });
+        });
+
+    }
+
+    // LAPTOP DETAIL
+    else if (pathName === '/laptop' && id < laptopData.length) {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+
+        fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
+            const laptop = laptopData[id];
+            const output = replaceTemplate(data, laptop);
+            res.end(output);
+        });
+    }
+
+    // IMAGES
+    else if ((/\.(jpg|jpeg|png|gif)$/i).test(pathName)) {
+        fs.readFile(`${__dirname}/data/img${pathName}`, (err, data) => {
+            res.writeHead(200, {
+                'Content-type': 'img/jpg'
+            });
+            res.end(data);
+        });
+    }
+
+    // URL NOT FOUND
+    else {
+        res.writeHead(404, {
+            'Content-type': 'text/html'
+        });
+        res.end('URL was not found on the server!');
+    }
+
+});
+
+
+server.listen(port, () => { // port, ip(localhost)
+    console.log('Listening for requests now');
+});
+
+
+function replaceTemplate(originalHTML, laptop) {
+    let output = originalHTML.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%ID%}/g, laptop.id);
+
+    return output;
+}
